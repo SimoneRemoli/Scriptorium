012d4e6681c654444b848d2e42ddd55e5e4e4141
Simone Remoli <simoneremoli00@gmail.com>
2025-03-16 20:24:58 +0100

    Add copy

--- src/it/uniroma2/dicii/bd/Model/DAO/AggiungiCopiaDAO.java
+++ src/it/uniroma2/dicii/bd/Model/DAO/AggiungiCopiaDAO.java
@@ -23,9 +23,12 @@ public class AggiungiCopiaDAO
             cs.executeQuery();
         }catch(SQLException e)
         {
+            if(e.getSQLState().equals("45010"))
+            {
+                System.out.println("Il libro non esiste, figuriamoci se posso aggiungere una copia");
+            }
             throw new RuntimeException(e);
         }
-
 
     }
 }
