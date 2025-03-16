3fca724dad763dc04935ca7e353acc6f116505d8
Simone Remoli <simoneremoli00@gmail.com>
2025-03-16 18:24:49 +0100

    Add copy

--- src/it/uniroma2/dicii/bd/Model/DAO/AggiungiCopiaDAO.java
+++ src/it/uniroma2/dicii/bd/Model/DAO/AggiungiCopiaDAO.java
@@ -2,6 +2,9 @@ package it.uniroma2.dicii.bd.Model.DAO;
 
 import it.uniroma2.dicii.bd.Exception.DAOException;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AggiungiCopiaDAO
@@ -11,6 +14,17 @@ public class AggiungiCopiaDAO
         Scanner tastiera = new Scanner(System.in);
         System.out.println("Inserisci il titolo della copia di un libro da aggiungere alla disponibilità:  ");
         String nome_libro = tastiera.nextLine();
+
+        try
+        {
+            Connection conn = ConnectionFactory.getConnection();
+            CallableStatement cs = conn.prepareCall("{call Aggiungi_Copia(?)}");
+            cs.setString(1,nome_libro);
+            cs.executeQuery();
+        }catch(SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
 
 
     }
