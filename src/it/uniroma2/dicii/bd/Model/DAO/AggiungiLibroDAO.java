3b3a097acb6473f620e12a19f04c0c34f4c7dd36
Simone Remoli <simoneremoli00@gmail.com>
2025-03-17 15:46:10 +0100

    Add book 1.1

--- src/it/uniroma2/dicii/bd/Model/DAO/AggiungiLibroDAO.java
+++ src/it/uniroma2/dicii/bd/Model/DAO/AggiungiLibroDAO.java
@@ -5,6 +5,7 @@ import it.uniroma2.dicii.bd.Exception.DAOException;
 import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -13,7 +14,10 @@ public class AggiungiLibroDAO
     public void Execute(Object... params) throws DAOException
     {
         Scanner tastiera = new Scanner(System.in);
-        String titolo="", isbn="";
+        String titolo="", isbn="",nome_scaffale="";
+        int indice = 0;
+        boolean check = false;
+        int categoria = 0,codice_scaffale=0,copie_attualmente_disponibili_book=0;
         try {
             System.out.println("Inserisci il titolo del nuovo libro: ");
             titolo = tastiera.nextLine();
@@ -28,15 +32,65 @@ public class AggiungiLibroDAO
         try{
 
             new MostraCategorieDAO().Execute();
+
             Connection conn = ConnectionFactory.getConnection();
-            CallableStatement cs = conn.prepareCall("{call Aggiungi_Libro(?,?)}");
+            System.out.println("Inserisci la categoria da attribuire al libro: ");
+            categoria = tastiera.nextInt();
+            CallableStatement cs = conn.prepareCall("{call aggiungi_libro(?,?,?)}");
             cs.setString(1,titolo);
             cs.setString(2,isbn);
-            cs.executeQuery();
+            cs.setInt(3,categoria);
+            check = cs.execute();
+
+            while(check)
+            {
+                ResultSet rs =  cs.getResultSet();
+                indice++;
+                if(rs!=null) {
+
+                    if (indice == 1)
+                    {
+                        while(rs.next())
+                        {
+                            nome_scaffale = rs.getString(1);
+                            codice_scaffale = rs.getInt(2);
+                        }
+                    }
+                    if (indice == 2)
+                    {
+                        while(rs.next())
+                        {
+                            copie_attualmente_disponibili_book = rs.getInt(1);
+                        }
+                    }
+                }
+                check = cs.getMoreResults();
+            }
+
         }catch(SQLException e)
         {
+            if(e.getMessage().equals("45012"))
+            {
+                System.out.println("Categoria inesistente.");
+            }
+            if(e.getMessage().equals("45013"))
+            {
+                System.out.println("Libro già esistente.");
+            }
             throw new RuntimeException(e);
         }
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("Il nuovo libro con titolo ' "+titolo+" ' e ISBN = "+isbn+", verrà aggiunto allo " +
+                "scaffale numero '"+codice_scaffale+"' denominato '"+ nome_scaffale+ "' che gestisce la categoria" +
+                " con codice "+ categoria+";");
+        System.out.println("Attualmente ci sono "+copie_attualmente_disponibili_book+" copie disponibili nel magazzino del libro appena aggiunto.");
+        System.out.println("------------------------------------------------------------------------");
+    new RegistraLibroECopiaDAO().Execute(isbn, codice_scaffale);
+
+
+
+
+
 
 //la funzione deve mostrare anche tutte le categorie con la relativa descrizione.
 
---
+++ src/it/uniroma2/dicii/bd/Model/DAO/RegistraLibroECopiaDAO.java
@@ -0,0 +1,36 @@
+package it.uniroma2.dicii.bd.Model.DAO;
+
+import it.uniroma2.dicii.bd.Exception.DAOException;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class RegistraLibroECopiaDAO
+{
+    public void Execute(Object... params) throws DAOException
+    {
+        String isbn = (String)params[0];
+        int codice_scaffale = (int) params[1], codice_ripiano = 0;
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("Inserisci il codice del ripiano dove porre il libro:  ");
+        codice_ripiano = tastiera.nextInt();
+
+        try{
+
+            Connection conn = ConnectionFactory.getConnection();
+            CallableStatement cs = conn.prepareCall("call registra_libro_e_copia(?,?,?)}");
+            cs.setString(1,isbn);
+            cs.setInt(2,codice_scaffale);
+            cs.setInt(3,codice_ripiano);
+            cs.executeQuery();
+        }catch(SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+}
