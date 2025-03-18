bd8fd627633182d164518fc4c3c7a2db53c118e7
Simone Remoli <simoneremoli00@gmail.com>
2025-03-18 09:55:54 +0100

    Report who done

--- src/it/uniroma2/dicii/bd/Model/DAO/InformazioniPossessoDAO.java
+++ src/it/uniroma2/dicii/bd/Model/DAO/InformazioniPossessoDAO.java
@@ -4,6 +4,7 @@ import it.uniroma2.dicii.bd.Exception.DAOException;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class InformazioniPossessoDAO
@@ -15,7 +16,18 @@ public class InformazioniPossessoDAO
 
             Connection conn = ConnectionFactory.getConnection();
             CallableStatement cs = conn.prepareCall("{call report_possesso()}");
-            cs.executeQuery();
+
+            ResultSet rs =  cs.executeQuery();
+
+            while(rs.next())
+            {
+                System.out.println("-----------------------------------------------------------------------------------------------------------");
+                System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getString(4)+"\t"+rs.getDate(5)+"\t"+rs.getString(6)+
+                        "\t"+rs.getString(7)+"\t"+rs.getString(8));
+                System.out.println("-----------------------------------------------------------------------------------------------------------");
+
+            }
+
         }catch(SQLException e)
         {
             throw new RuntimeException(e.getMessage());
