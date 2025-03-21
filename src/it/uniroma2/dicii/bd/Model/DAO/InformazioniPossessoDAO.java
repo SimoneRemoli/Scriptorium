e1eeecdfe9cdb4c1024372cd2806c572f60c09e0
Simone Remoli <simoneremoli00@gmail.com>
2025-03-21 11:33:47 +0100

    Fixing cursor

--- src/it/uniroma2/dicii/bd/Model/DAO/InformazioniPossessoDAO.java
+++ src/it/uniroma2/dicii/bd/Model/DAO/InformazioniPossessoDAO.java
@@ -26,29 +26,36 @@ public class InformazioniPossessoDAO
                 ResultSet rs = cs.getResultSet();
                 if(rs!=null)
                 {
-                    if(indice==1)
-                    {
+                    if (indice == 1) {
                         System.out.println("\n\n\t Gestione prestiti in corso locali. \n");
-                        while(rs.next())
-                        {
-                            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-                            System.out.println("Numero copia:"+rs.getInt(1)+"\t Nome:"+rs.getString(2)+"\t Cognome:"+rs.getString(3)+"\t Titolo:"+rs.getString(4)+"\t Data di nascita:"+rs.getDate(5)+"\t Isbn:"+rs.getString(6)+
-                                    "\t Numero di telefono:"+rs.getString(7)+"\t Tipologia contatto preferita:"+rs.getString(8));
-                            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
+                        if (!rs.next()) {
+                            System.out.println("Non ci sono prestiti locali in corso.");
+                        } else {
+                            do {
+                                System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                                System.out.println("Numero copia:" + rs.getInt(1) + "\t Nome:" + rs.getString(2) + "\t Cognome:" + rs.getString(3) + "\t Titolo:" + rs.getString(4) + "\t Data di nascita:" + rs.getDate(5) + "\t Isbn:" + rs.getString(6) +
+                                        "\t Numero di telefono:" + rs.getString(7) + "\t Tipologia contatto preferita:" + rs.getString(8));
+                                System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                            } while (rs.next());
                         }
                     }
                     if(indice==2)
                     {
-                        System.out.println("\n\n\t Gestione prestiti in corso di altre biblioteche. \n");
-                        while(rs.next())
-                        {
-                            System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-                            System.out.println("Nome:"+rs.getString(1)+"\t Cognome: "+rs.getString(2)+"\t Titolo:"+rs.getString(3)+"\t Data di nascita: "+rs.getString(4)+"\t Isbn:"+rs.getString(5)+"\t Numero di telefono:"+rs.getString(6)+
-                                    "\t Tipologia contatto preferita:"+rs.getString(7)+"\t Copia reperita da:"+rs.getString(8)+"\t Presso:"+rs.getString(9));
-                            System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                        if (!rs.next()) { //altrimenti avanza il cursore e me salta il primo elemento
+                            System.out.println("Non ci sono prestiti in corso di libri richiesti in altre biblioteche.");
+                        } else {
+                            System.out.println("\n\n\t Gestione prestiti in corso di altre biblioteche. \n");
 
+                            do {
+                                System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                                System.out.println("Nome:" + rs.getString(1) + "\t Cognome: " + rs.getString(2) + "\t Titolo:" + rs.getString(3) + "\t Data di nascita: " + rs.getString(4) + "\t Isbn:" + rs.getString(5) + "\t Numero di telefono:" + rs.getString(6) +
+                                        "\t Tipologia contatto preferita:" + rs.getString(7) + "\t Copia reperita da:" + rs.getString(8) + "\t Presso:" + rs.getString(9));
+                                System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+                            }while (rs.next());
                         }
+
                     }
                 }
                 System.out.println("\n");
