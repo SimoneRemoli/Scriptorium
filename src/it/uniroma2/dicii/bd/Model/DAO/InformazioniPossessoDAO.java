1b46db5d38d414dfc1776289acb3c1d33c0d9750
Simone Remoli <simoneremoli00@gmail.com>
2025-03-21 11:09:57 +0100

    Duplicate Result Set for bibliotecario

--- src/it/uniroma2/dicii/bd/Model/DAO/InformazioniPossessoDAO.java
+++ src/it/uniroma2/dicii/bd/Model/DAO/InformazioniPossessoDAO.java
@@ -17,16 +17,46 @@ public class InformazioniPossessoDAO
             Connection conn = ConnectionFactory.getConnection();
             CallableStatement cs = conn.prepareCall("{call report_possesso()}");
 
-            ResultSet rs =  cs.executeQuery();
+            boolean status = cs.execute();
+            int indice = 0;
 
-            while(rs.next())
+            while(status)
             {
-                System.out.println("-----------------------------------------------------------------------------------------------------------");
-                System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getString(4)+"\t"+rs.getDate(5)+"\t"+rs.getString(6)+
-                        "\t"+rs.getString(7)+"\t"+rs.getString(8));
-                System.out.println("-----------------------------------------------------------------------------------------------------------");
+                indice++;
+                ResultSet rs = cs.getResultSet();
+                if(rs!=null)
+                {
+                    if(indice==1)
+                    {
+                        System.out.println("\n\n\t Gestione prestiti in corso locali. \n");
+                        while(rs.next())
+                        {
+                            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                            System.out.println("Numero copia:"+rs.getInt(1)+"\t Nome:"+rs.getString(2)+"\t Cognome:"+rs.getString(3)+"\t Titolo:"+rs.getString(4)+"\t Data di nascita:"+rs.getDate(5)+"\t Isbn:"+rs.getString(6)+
+                                    "\t Numero di telefono:"+rs.getString(7)+"\t Tipologia contatto preferita:"+rs.getString(8));
+                            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+                        }
+                    }
+                    if(indice==2)
+                    {
+                        System.out.println("\n\n\t Gestione prestiti in corso di altre biblioteche. \n");
+                        while(rs.next())
+                        {
+                            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                            System.out.println("Nome:"+rs.getString(1)+"\t Cognome: "+rs.getString(2)+"\t Titolo:"+rs.getString(3)+"\t Data di nascita: "+rs.getString(4)+"\t Isbn:"+rs.getString(5)+"\t Numero di telefono:"+rs.getString(6)+
+                                    "\t Tipologia contatto preferita:"+rs.getString(7));
+                            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+                        }
+                    }
+                }
+                System.out.println("\n");
+                status = cs.getMoreResults();
 
             }
+
+
 
         }catch(SQLException e)
         {
